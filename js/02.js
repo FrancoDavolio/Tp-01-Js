b47616d64b5ea613b4c75b9ab556030c0803f7b3
Franco Davolio <francodavolio12@gmail.com>
2022-07-13 20:34:59 -0300

    terminado el ejercicio 02

--- js/02.js
+++ js/02.js
@@ -1,19 +1,25 @@
 let nota = parseInt(prompt("Ingresar una nota en números del 0 al 10"));
 
 switch (nota) {
-    case 0,1,2:
+    case 0:
+    case 1:
+    case 2:
         alert("Muy deficiente");
         break;
-    case 3,4:
+    case 3:
+    case 4:
         alert("Insuficiente");
         break;
-    case 5,6:
+    case 5:
+    case 6:
         alert("Insuficiente");
         break;
-    case 7,8:
+    case 7:
+    case 8:
         alert("Insuficiente");
         break;
-    case 9,10:
+    case 9:
+    case 10:
         alert("Insuficiente");
         break;
     default:
