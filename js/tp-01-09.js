4da7d5015a431e76bf6f21d14af320455cf457df
Franco Davolio <francodavolio12@gmail.com>
2022-07-13 19:02:22 -0300

    tp-01-09 listo

---
+++ js/tp-01-09.js
@@ -0,0 +1,14 @@
+let frase = prompt("Escribe una frase");
+let nfrase = frase.length;
+
+for (i = 0; i <= nfrase; i++) {
+  if (
+    frase.charAt(i, 1) === "a" ||
+    frase.charAt(i, 1) === "e" ||
+    frase.charAt(i, 1) === "i" ||
+    frase.charAt(i, 1) === "o" ||
+    frase.charAt(i, 1) === "u"
+  ) {
+    document.write(frase.charAt(i, 1));
+  }
+}
