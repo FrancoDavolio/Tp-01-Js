eabe4e123408d959289634cf2e701d7139f7126b
Franco Davolio <francodavolio12@gmail.com>
2022-07-13 20:49:30 -0300

    falta corregir el error de la (-) y que se repite la primera palabra

---
+++ js/03.js
@@ -0,0 +1,7 @@
+let fraseCompleta = ""
+
+while(confirm("¿Desea ingresar palabras?")){
+    let textoIngresado = prompt("Ingrese una palabra");
+    fraseCompleta = fraseCompleta + textoIngresado + "-"
+    document.write(fraseCompleta)
+}
